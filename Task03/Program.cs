ea97b249f98d700bc4b62e14de3c65b7afd5ae19
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-27 22:00:51 +0300

    add 3 Tasks

---
+++ Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+
+Clear();
+
+Write("Введите массив через пробел: ");
+
+int[] array = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+.Select(x => int.Parse(x)).Where(x => x % 2 == 0).ToArray();
+WriteLine(String.Join(", ", array));
